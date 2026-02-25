5a761c7700a327a3c9ee3bfff65fb53c62219c3f
hazem <zomatarek@14gmail.com>
2026-02-25 13:43:42 +0200

    Added base images - styles - colors

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,19 @@
+abstract final class AppAssets {
+  static const _base = 'assets/images/';
+
+  static const String logoPng     = '${_base}Logo.png';
+  static const String mosahf      = '${_base}mosahf_image.png';
+
+  static const String icHadeth    = '${_base}ic_hadeth.png';
+  static const String icRadio     = '${_base}ic_radio.png';
+  static const String icSebha     = '${_base}ic_sebha.png';
+  static const String icTime      = '${_base}ic_time.png';
+  static const String icQuran      = '${_base}ic_quran.png';
+
+  static const String ahadethTab  = '${_base}ahadeth_tab_bg.png';
+  static const String leftCornerImg  = '${_base}img_left_corner.png';
+  static const String rightCornerImg = '${_base}img_right_corner.png';
+  static const String mosque      = '${_base}mosque_img.png';
+  static const String quranTab    = '${_base}quran_tab_bg.png';
+  static const String splash      = '${_base}Splash_image.png';
+}
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:islami_i/utils/app_colors.dart';
+
+abstract final class AppStyles{
+  static const TextStyle whiteBold20 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.white,
+      fontSize:20
+  );
+  static const TextStyle whiteBold14 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.white,
+      fontSize:14
+  );
+  static const TextStyle whiteBold16 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.white,
+      fontSize:16
+  );
+  static const TextStyle goldBold20 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.gold,
+      fontSize:20
+  );
+  static const TextStyle lightBlackBold16 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.lightBlack,
+      fontSize:16
+  );
+  static const TextStyle lightBlackBold24 = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: AppColors.lightBlack,
+      fontSize:24
+  );
+}
